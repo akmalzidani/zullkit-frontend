54e58af06733faf72f0c590ab34c0b1cfd1bcc3e
akmalzidani <akmal20031003@gmail.com>
2024-07-29 14:24:56 +0700

    update tailwind

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -17,7 +17,7 @@ onMounted(() => {
 
 <template>
   <div>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
       <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
         <Logo />
         <NavigationLinks />
